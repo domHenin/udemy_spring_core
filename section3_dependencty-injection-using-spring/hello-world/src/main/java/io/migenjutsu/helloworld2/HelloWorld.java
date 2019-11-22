7541a20a67e6b6ff5bacffcc6c25eb376ad204b6
domHenin <domhenin.517@gmail.com>
2019-11-22 15:02:42 -0500

    worked on dependency injecting using @Autowired annotation

--- section3_dependencty-injection-using-spring/hello-world/src/main/java/io/migenjutsu/helloworld2/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package io.migenjutsu.helloworld2;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class HelloWorld implements HelloWorldService {
-    
-}
---
+++ section3_dependencty-injection-using-spring/hello-world/src/main/java/io/migenjutsu/helloworld3/SetterBasedService.java
@@ -0,0 +1,30 @@
+package io.migenjutsu.helloworld3;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SetterBasedService {
+    private HelloWorldService helloWorldService;
+
+    @Autowired
+    public void SetterBasedService(HelloWorldService helloWorldService) {
+        this.helloWorldService=helloWorldService;
+    }
+
+    public void getMessage() {
+        helloWorldService.sayHello();
+    }
+}
+
+
+// private HelloWorldService helloWorldService;
+
+// @Autowired
+// public void setHelloWorldService(HelloWorldService helloWorldService) {
+//     this.helloWorldService=helloWorldService;
+// }
+
+// public void getMessage() {
+//     helloWorldService.sayHello();
+// }
